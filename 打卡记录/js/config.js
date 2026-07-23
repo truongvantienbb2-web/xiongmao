7521b32d7dbc60a2e6918451441e543e992a7809
truongvantienbb2-web <truongvantienbb2@gmail.com>
2026-07-23 17:57:00 +0800

    Delete 打卡记录 directory

--- 打卡记录/js/config.js
+++
@@ -1,6 +0,0 @@
-// JSONBin 配置
-// 班级群分享用，Master Key 会暴露在页面源码中
-const CONFIG = {
-  BIN_ID: '6a5cdfe2f5f4af5e29a4190d',
-  MASTER_KEY: '$2a$10$kATiFmgMtVmJnHiyQmZSc.y7lq1qVlTnryCkSXrYDCNKBKEfo8SKa'
-};
--- 打卡记录/打卡记录/js/config.js
+++
@@ -1,6 +0,0 @@
-// JSONBin 配置
-// 班级群分享用，Master Key 会暴露在页面源码中
-const CONFIG = {
-  BIN_ID: '6a61db9ada38895dfe8373c2',
-  MASTER_KEY: '$2a$10$kATiFmgMtVmJnHiyQmZSc.y7lq1qVlTnryCkSXrYDCNKBKEfo8SKa'
-};
